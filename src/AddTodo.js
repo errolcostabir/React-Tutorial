3bcf71dbebfbb85204e1ec76193c2abfbdf9566b
errolcostabir <errolcostabir98@gmail.com>
2020-03-13 14:49:05 +0530

    initial commit

---
+++ src/AddTodo.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+
+class AddTodo extends Component {
+
+    state = {
+        item: ''
+
+    }
+    handleChange = (e) => {
+        this.setState({
+            item: e.target.value
+
+        })
+    }
+
+    handleSubmit=(e)=>{
+        e.preventDefault();
+        this.props.addTodo(this.state);
+        this.setState({
+            item: ''
+        })
+    }
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Add New Todo: </label>
+                    <input type="text" onChange={this.handleChange} value={this.state.item} />
+                </form>
+            </div>
+        )
+    }
+}
+
+
+export default AddTodo;

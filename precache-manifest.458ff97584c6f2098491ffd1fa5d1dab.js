0cb2f7f8b8094f820821d0667182d1f0cf0bebfe
errolcostabir <errolcostabir98@gmail.com>
2020-03-13 17:04:55 +0530

    Updates

---
+++ precache-manifest.458ff97584c6f2098491ffd1fa5d1dab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97a9e8e2cd3272f2de104bc095c7e678",
+    "url": "/React-Tutorial/index.html"
+  },
+  {
+    "revision": "990fbf90d1e932e110d6",
+    "url": "/React-Tutorial/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "96c3cc8c2573f486344a",
+    "url": "/React-Tutorial/static/js/2.41969981.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React-Tutorial/static/js/2.41969981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "990fbf90d1e932e110d6",
+    "url": "/React-Tutorial/static/js/main.820a0568.chunk.js"
+  },
+  {
+    "revision": "c6cdeb0f7017c849dfbc",
+    "url": "/React-Tutorial/static/js/runtime-main.01e69ece.js"
+  }
+]);
